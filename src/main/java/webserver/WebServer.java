c26655470bfd2ca109f0febc317ecf7ca6a2a520
Song-EunJu <jd06280@naver.com>
2024-01-23 23:07:11 +0900

    add : WebServer 클래스 주석 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -19,11 +19,13 @@ public class WebServer {
         }
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-
+        // 웹 서버를 시작하고
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
+            // 사용자의 요청 전까지 대기 상태, 사용자 요청 들어오면 RequestHandler 클래스에 위임
+            // 사용자 요청 발생할 때까지 대기 상태에 있도록 지원하는 역할 -> ServerSocket
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
                 RequestHandler requestHandler = new RequestHandler(connection);
