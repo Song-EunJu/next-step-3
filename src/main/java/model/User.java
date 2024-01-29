2a4dd111db94f8988d28331e987d5778a68ff0ab
Song-EunJu <jd06280@naver.com>
2024-01-29 22:22:02 +0900

    add : Setter, 기본 생성자 어노테이션 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,10 @@
 package model;
 
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
 public class User {
     private String userId;
     private String password;
