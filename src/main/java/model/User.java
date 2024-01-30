06269f325095f229eae52e072a4670699ba15fa7
Song-EunJu <jd06280@naver.com>
2024-01-30 22:53:49 +0900

    mod : User 클래스 어노테이션 추가 및 메서드 삭제

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,41 +1,24 @@
 package model;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.ToString;
 
-@Setter
+@Getter
 @NoArgsConstructor
+@ToString
 public class User {
     private String userId;
     private String password;
     private String name;
     private String email;
 
+    @Builder
     public User(String userId, String password, String name, String email) {
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public String toString() {
-        return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
     }
 }
